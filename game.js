486e940ec7d32639fa992f85a55e56b876de6007
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-01-16 21:51:18 +0530

    removed animation of  bubbles

--- game.js
+++ game.js
@@ -1,19 +1,19 @@
 const handlePop= function(event){
   const id = event.target.id;
-  const element = document.getElementById(id);
   const parent = document.getElementsByClassName('bubbleBox')[0];
-  parent.removeChild(element);
+  const bubblePop = document.getElementById('pop');
+  bubblePop.play();
+  parent.removeChild(event.target);
 }
 
 const addBubbles = function(gameBoard){
-  for (i=0;i<20;i++){ 
+  for (i=0;i<10;i++){ 
     const bubble = document.createElement('div');
     bubble.className = 'bubble';
-    posX =650;
-    posY =650;
-    bubble.style.height= Math.random()*100+'px';
+    bubble.style.height= 50+Math.random()*10+'px';
     bubble.style.width = bubble.style.height;
-    bubble.style.left = Math.random()*1000+'px';
+    bubble.style.left = 250+(Math.random()*1000)%800+'px';
+    bubble.style.top = 200+ (Math.random()*500)%350 +'px';
     bubble.setAttribute('id',i);
     bubble.addEventListener('mouseover',handlePop);
     gameBoard.append(bubble);
@@ -21,7 +21,6 @@ const addBubbles = function(gameBoard){
 }
 main = function(){
   const gameBoard = document.getElementsByClassName('bubbleBox')[0];
-  console.log(gameBoard);
-  setInterval(addBubbles, 5*1000,gameBoard)
-  
+  addBubbles(gameBoard);
+  setInterval(addBubbles,3*1000,gameBoard);
 }
